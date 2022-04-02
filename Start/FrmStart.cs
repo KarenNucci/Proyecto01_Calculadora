40188286414776479d5a53def461961bd7e6f041
Karen Micaela Nucci <karen.nucci27@gmail.com>
2022-04-02 20:55:09 -0300

    Creando Forms de Calculadora simple

--- Start/FrmStart.cs
+++ Start/FrmStart.cs
@@ -7,19 +7,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Calculator;
 
 namespace Start
 {
     public partial class FrmStart : Form
     {
-        public FrmStart()
-        {
-            InitializeComponent();
-        }
+        public FrmStart() { InitializeComponent(); }
 
         private void calculatorToolStripMenuItem_Click(object sender, EventArgs e)
         {
-          
+            FrmCalculator calc = new FrmCalculator();
+            calc.Show();
+            this.Close();
         }
     }
 }
